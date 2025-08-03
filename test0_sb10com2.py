c25ba268947980f61325f19f4e2c6a33f697e410
Zsuyang <89232166+Zsuyang@users.noreply.github.com>
2025-08-03 10:33:09 +0800

    Update test0_sb10com2.py

--- test0_sb10com2.py
+++ test0_sb10com2.py
@@ -1,27 +1,25 @@
 '''
-5 次结果汇总求平均值
+5次结果 求平均值
 
 '''
 import csv
-
 import pandas as pd
-
 import MultiPMeasurs
 import numpy as np
 import sklearn.metrics as sk
 
 
 def method(clf, name):
-	fileList =  ['eco','shu','yea',
-		]
+	fileList =['eco','shu','yea',]
 
 	with open(
-		'pic/{}_eco/{}.csv'.format(name, clf),
+		'Comparison/{}/{}.csv'.format(name, clf),
 		'a') as f:  # 方法(记得同步修改写入文件名称)
 		csv_writer = csv.writer(f)
 		csv_writer.writerow(
 			['Data', "F1", 'GMEAN', 'Gmean1', 'Gmean2', 'Mava', 'MAUC',
-			 'Precision', 'Specificity', 'Recall', 'preclass_recall'\
+			 'Precision', 'Specificity', 'Recall', 'preclass_recall',
+			 'f1_pre_class'\
 			 ])
 		for fn in fileList:
 			F1 = []
@@ -34,10 +32,11 @@ def method(clf, name):
 			Mava = []
 			MAUC = []
 			Recall_pre = []
-			for z in range(1,6):
+			Pre_class_f1=[]
+			for z in range(1, 6):
 				# print(z)
 				# for j in range(1,6):
-				for i in range(1,6):
+				for i in range(1, 6):
 					# print('i=',i)
 					df_pred = pd.read_csv(
 						'{}_TestData/{}Result/{}_{}/{}/{}_label.csv'.format(
@@ -56,20 +55,23 @@ def method(clf, name):
 					# print(label)
 					# print('{}_TestData/Result/{}_{}/{}_sigma={}/{}_label.csv'.format(z, z, i,clf,k,fn) )
 					recall1, precision, gmean1 = MultiPMeasurs.calculate_micro_gmean1(
-						label, prd)  
+						label, prd)  # recall1,recall2，macro_recall 一样
 					recall2, specificity, gmean2 = MultiPMeasurs.calculate_micro_gmean2(
 						label, prd)
-					pre_recall,macro_recall, macro_precision, gmean_score = \
+					pre_recall,macro_recall, macro_precision, gmean_score,\
+                    f1_scores_eachclass = \
 						MultiPMeasurs.calculate_macro_gmean(
-						label, prd)
+						label, prd)#
+
 					f1_score = sk.f1_score(label, prd, average = 'macro')
 					#print('pre_recall',pre_recall,macro_recall,)
-					
+			
+
 
 
 					mava = MultiPMeasurs.MAvA(label, prd)
 
-					
+				
 					unique_classes = np.unique ( label )
 					#print ( "y_true shape:" , label.shape )  # 应为 (n_samples,)
 					#print ( "y_pred_proba shape:" ,
@@ -88,12 +90,18 @@ def method(clf, name):
 					Mava.append(mava)
 					MAUC.append(mauc)
 					Recall_pre.append ( pre_recall ) #[类别,样本数]
+					Pre_class_f1.append(f1_scores_eachclass)
 
 					#print(Recall_pre,len(Recall_pre),len(label))
 			column_avgs = [ sum ( column ) / len ( column ) for column in
 					zip ( *Recall_pre ) ]
 			formatted_avgs = [f"{aa * 100:.4f}" for aa in column_avgs ]
-			#print ( column_avgs )
+
+			column_Pre_class_f1 = [ sum ( column ) / len ( column ) for column in
+							zip ( *Pre_class_f1 ) ]
+			formatted_column_Pre_class_f1 = [ f"{aa * 100:.4f}" for aa in column_Pre_class_f1 ]
+
+			#print ( formatted_column_Pre_class_f1 )
 			csv_writer.writerow(
 				[fn, str(round(np.average(F1), 4) * 100),
 				 str(round(np.average(GMEAN), 4) * 100),
@@ -103,18 +111,16 @@ def method(clf, name):
 				 str(round(np.average(MAUC), 4) * 100),
 				 str(round(np.average(Precision), 4) * 100),
 				 str(round(np.average(Specificity), 4) * 100),
-				 str(round(np.average(Recall), 4) * 100), *formatted_avgs]
+				 str(round(np.average(Recall), 4) * 100), *formatted_avgs,*formatted_column_Pre_class_f1]
 			)
 
 
 
 if __name__ == '__main__':
-	name = 'AMG2_'  #  KDN
-	
-	# Omether(name)
-	list = [ 'mlp_0.3','CART_0.3' ]  #
-	clf = 'CART'
-	# F1,Recall,Precision,Specificity,GMEAN,Gmean1,Gmean2,Mava ,MAUC=
-	for clf in list :
-		method ( clf , name = name)
+	name = 'AMG2_'  # KDN
+
+	list = ['mlp_0.3','CART_0.3']
+	for clf in list:
+		method(clf, name = name)
+
 
